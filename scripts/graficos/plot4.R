cf0cf24c73007e86b84e3cffa3bd4047d8b339cd
CrisOrdaz <cordaz@ucm.es>
2026-05-28 19:26:41 +0200

    Update plot4.R

--- scripts/graficos/plot4.R
+++ scripts/graficos/plot4.R
@@ -36,7 +36,7 @@ NIVELES <- c(110, 120, 130)
 
 cargar_semilla <- function(semilla) {
   path <- file.path(RESULTS_ROOT, paste0("semilla_", semilla),
-                    "elastic_net_results_FUSIONADO.rds")
+                    "elastic_net_results_FINAL.rds")
   df <- readRDS(path)
   df$semilla_split <- semilla
   df
